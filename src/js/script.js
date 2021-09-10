40865f17ae18a604b325cb59bb723ef9909b52a1
zsh-dev <andiec148@gmail.com>
2021-09-10 16:15:52 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,77 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.portfolio__slider-btn_next',
+    prevEl: '.portfolio__slider-btn_prev',
+  },
+
+});
+
+function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+  const elems = document.querySelectorAll(selector);
+
+  function mask(event) {
+    const keyCode = event.keyCode;
+    const template = masked,
+      def = template.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, "");
+    console.log(template);
+    let i = 0,
+      newValue = template.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+      });
+    i = newValue.indexOf("_");
+    if (i !== -1) {
+      newValue = newValue.slice(0, i);
+    }
+    let reg = template.substr(0, this.value.length).replace(/_+/g,
+      function (a) {
+        return "\\d{1," + a.length + "}";
+      }).replace(/[+()]/g, "\\$&");
+    reg = new RegExp("^" + reg + "$");
+    if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+      this.value = newValue;
+    }
+    if (event.type === "blur" && this.value.length < 5) {
+      this.value = "";
+    }
+
+  }
+
+  for (const elem of elems) {
+    elem.addEventListener("input", mask);
+    elem.addEventListener("focus", mask);
+    elem.addEventListener("blur", mask);
+  }
+
+}
+maskPhone('input[name="number"]', '+7 (___) ___-__-__');
+
+const contactsForm = document.querySelector('.contacts__form'),
+  policyBtn = document.querySelectorAll('.form__policy-btn');
+contactsForm.addEventListener('click', ({
+  target
+}) => {
+  if (target.classList.contains('form__policy-btn') || target.classList.contains('form__policy-text')) {
+    policyBtn.forEach((item) => {
+      item.classList.toggle('active');
+    })
+
+  }
+})
+
+
+
+const burger = document.querySelector('.burger'),
+  menu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  menu.classList.toggle('active');
+  document.body.classList.toggle('lock');
+})
+
+
+
+AOS.init({disable: 'mobile'});
